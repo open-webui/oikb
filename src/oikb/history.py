b4d4a4267751938ef34a363f7eef23184c188218
rechardp <rechardp@maluti-x.com>
2026-06-30 11:40:00 +0200

    Updated the correct http request to follow redirects when reading files from SharePoint. This ensures that the content is retrieved correctly even if the file has been moved or renamed.

--- src/oikb/history.py
+++ src/oikb/history.py
@@ -45,7 +45,7 @@ class SyncHistory:
 
     def _get_conn(self) -> sqlite3.Connection:
         if self._conn is None:
-            self._conn = sqlite3.connect(str(self.db_path))
+            self._conn = sqlite3.connect(str(self.db_path), check_same_thread=False)
             self._conn.row_factory = sqlite3.Row
             self._conn.executescript(_SCHEMA)
         return self._conn
